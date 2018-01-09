03473addc2dfae7b0430d2d0e4e81769b1be352f
Stephen Pearce <spearce@me.com>
2018-01-10 10:52:50 +1100

    Working with live data

---
+++ process_matches.R
@@ -0,0 +1,93 @@
+library(htmltools)
+library(tidyverse)
+library(nufflytics)
+
+## functions -----
+make_ticker <- function(match_data) {
+  
+  div(class = "news",
+      tags$ul(
+        tags$li(
+          make_game_summary(match_data[[1]])
+        ),
+        tags$li(
+          make_game_summary(match_data[[2]])
+        ),
+        tags$li(
+          make_game_summary(match_data[[3]])
+        ),
+        tags$li(
+          make_game_summary(match_data[[4]])
+        )
+      )
+  )
+  
+}
+
+make_game_summary <- function(md) {
+  
+  match_table <- data.frame(
+    home = team_summary(md$teams[[1]], is_home = T),
+    separators = c(
+      img(src="img/BigIconVS.png", height = 10) %>% as.character(),
+      img(src="img/REBBL_s.png", height = 25, class="separator") %>% as.character(),
+      img(src="img/Casualty.png", height=20, class="separator") %>% as.character()
+    ),
+    away = team_summary(md$teams[[2]], is_home = F)
+  )
+  
+  
+  HTML(
+    knitr::kable(
+      match_table,
+      format="html", escape = F, col.names = NULL, align = "rcl"
+    )
+  )
+  
+}
+
+team_summary <- function(team_data, is_home) {
+  race = id_to_race(team_data$idraces) %>% str_replace_all(" ","")
+  
+  if(is_home) {
+    name = paste(team_data$teamname, img(src=glue::glue("img/race/{race}.png"), height = 20, class = "separator") %>% as.character)
+  } else {
+    name = paste(img(src=glue::glue("img/race/{race}.png"), height = 20, class = "separator") %>% as.character, team_data$teamname)
+  }
+  
+  TDs = team_data$inflictedtouchdowns
+  
+  CAS = team_data$sustainedcasualties
+  
+  if(team_data$sustaineddead > 0 ) {
+    if(is_home) {
+      CAS <- paste0("(",team_data$sustaineddead," ",img(src="img/Dead.png", height = 20, class = "separator") %>% as.character,") ",CAS)
+    } else {
+      CAS <- paste0(CAS," (",img(src="img/Dead.png", height = 20, class = "separator") %>% as.character," ",team_data$sustaineddead,")")
+    }
+  }
+  
+  c(name,TDs,CAS)
+}
+
+#load data -----
+
+load("data/key.rda")
+
+old_data <- readRDS("data/matches.rds")
+
+new_data <- api_matches(key, league = "Cabalvision Official League", competition = "Champion Ladder XI", limit = 4)
+
+new_uuids <- new_data$matches %>% map_chr("uuid")
+
+#replace data if newer matches available -----
+
+if(all(new_uuids != old_data$uuids)) {
+  
+  new_ticker <- make_ticker(new_data$matches)
+  
+  to_save <- list(uuids = new_uuids, ticker = new_ticker)
+  
+  saveRDS(to_save, file = "data/matches.rds")
+}
+
--- server.R
+++ server.R
@@ -4,44 +4,9 @@ library(magrittr)
 
 shinyServer(function(input, output, session) {
   
-  read_data <- function(file) {
-    read_lines(file) %>% as.list()
-  }
-  
-vals = reactiveFileReader(20000, session, "data/info.tsv", read_data)
+  data = reactiveFileReader(20000, session, "data/matches.rds", readRDS)
   
   output$ticker = renderUI({
-      div(class = "news",
-        tags$ul(
-          tags$li(
-            HTML(knitr::kable(data.frame(
-              c(paste("TeamA",vals()[[1]]), sample(1:4,1), sample(1:4,1)), 
-              c(img(src="img/BigIconVS.png", height = 10) %>% as.character(),img(src="img/REBBL_s.png", height = 25, class="separator") %>% as.character(),img(src="img/Casualty.png", height=20, class="separator") %>% as.character()), 
-              c(paste("TeamB",vals()[[1]]),sample(1:4,1), sample(1:4,1))
-              ), format="html", escape = F, col.names = NULL, align = "rcl"))
-            ),
-          tags$li(
-            HTML(knitr::kable(data.frame(
-              c(paste("TeamA",vals()[[2]]), sample(1:4,1), sample(1:4,1)), 
-              c(img(src="img/BigIconVS.png", height = 10) %>% as.character(),img(src="img/REBBL_s.png", height = 25, class="separator") %>% as.character(),img(src="img/Casualty.png", height=20, class="separator") %>% as.character()), 
-              c(paste("TeamB",vals()[[2]]),sample(1:4,1), sample(1:4,1))
-            ), format="html", escape = F, col.names = NULL, align = "rcl"))
-          ),
-          tags$li(
-            HTML(knitr::kable(data.frame(
-              c(paste("TeamA",vals()[[3]]), sample(1:4,1), sample(1:4,1)), 
-              c(img(src="img/BigIconVS.png", height = 10) %>% as.character(),img(src="img/REBBL_s.png", height = 25, class="separator") %>% as.character(),img(src="img/Casualty.png", height=20, class="separator") %>% as.character()), 
-              c(paste("TeamB",vals()[[3]]),sample(1:4,1), sample(1:4,1))
-            ), format="html", escape = F, col.names = NULL, align = "rcl"))
-          ),
-          tags$li(
-            HTML(knitr::kable(data.frame(
-              c(paste("TeamA",vals()[[4]]), sample(1:4,1), sample(1:4,1)), 
-              c(img(src="img/BigIconVS.png", height = 10) %>% as.character(),img(src="img/REBBL_s.png", height = 25, class="separator") %>% as.character(),img(src="img/Casualty.png", height=20, class="separator") %>% as.character()), 
-              c(paste("TeamA",vals()[[4]]),sample(1:4,1), sample(1:4,1))
-            ), format="html", escape = F, col.names = NULL, align = "rcl"))
-          )
-        )
-      )
+      data()$ticker
   })
 })
